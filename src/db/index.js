51ff592294cd34eb73d79857a4e68b6c68551a9f
Abhinav Singh <abhinavas430@gmail.com>
2023-12-30 22:46:27 +0530

    Set: ENV Proce

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 connect().catch((err) => console.log(err));
 
 async function connect() {
   //   await mongoose.connect("mongodb://127.0.0.1:27017/test");
 
-  const msg = await mongoose.connect(
-    "mongodb+srv://abhinavas430:Venom21@portfolio-cluster.1jxlhjm.mongodb.net/Portfolio",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  const msg = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   // console.log(msg);
 }
 
