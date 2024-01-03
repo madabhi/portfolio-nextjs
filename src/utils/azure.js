b8c5e3803f66d95c254d741b947964b4fe207883
Abhinav Singh <abhinavas430@gmail.com>
2024-01-03 07:07:59 +0530

    Fix Deployement Azure issues

--- src/utils/azure.js
+++ src/utils/azure.js
@@ -4,10 +4,7 @@ const { DefaultAzureCredential } = require("@azure/identity");
 const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 if (!accountName) throw Error("Azure Storage accountName not found");
 
-const blobServiceClient = new BlobServiceClient(
-  `https://${accountName}.blob.core.windows.net`,
-  new DefaultAzureCredential()
-);
+const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
 
 const containerClient = blobServiceClient.getContainerClient(
   process.env.AZURE_CONTAINER_NAME
