4c0d9b32864fabfe36965a91f06255d6eeac1570
Abhinav Singh <abhinavas430@gmail.com>
2024-01-03 08:28:28 +0530

    Fixed: Azure Working

--- src/utils/azure.js
+++ src/utils/azure.js
@@ -1,12 +1,28 @@
-import { BlobServiceClient } from "@azure/storage-blob";
+import {
+  BlobServiceClient,
+  StorageSharedKeyCredential,
+} from "@azure/storage-blob";
 const { DefaultAzureCredential } = require("@azure/identity");
 
 const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 if (!accountName) throw Error("Azure Storage accountName not found");
 
+// const blobServiceClient = new BlobServiceClient(
+//   `https://${accountName}.blob.core.windows.net`,
+//   new DefaultAzureCredential()
+// );
+
+const account = process.env.ACCOUNT_NAME || "";
+const accountKey = process.env.ACCOUNT_KEY || "";
+
+const sharedKeyCredential = new StorageSharedKeyCredential(
+  process.env.AZURE_STORAGE_ACCOUNT_NAME,
+  process.env.AZURE_STORAGE_CONNECTION_KEY
+);
+
 const blobServiceClient = new BlobServiceClient(
   `https://${accountName}.blob.core.windows.net`,
-  new DefaultAzureCredential()
+  sharedKeyCredential
 );
 
 const containerClient = blobServiceClient.getContainerClient(
