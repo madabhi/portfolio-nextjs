a0a918836db8f1bb1748290468c4b65ab7cb2ac9
Abhinav Singh <abhinavas430@gmail.com>
2024-01-19 14:31:44 +0530

    SEO Sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,25 +3,25 @@ export default function sitemap() {
     {
       url: "https://abhinavsingh-steel.vercel.app/",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "daily",
       priority: 1,
     },
     {
       url: "https://abhinavsingh-steel.vercel.app/about",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "daily",
       priority: 0.8,
     },
     {
       url: "https://abhinavsingh-steel.vercel.app/blog",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "daily",
       priority: 0.5,
     },
     {
       url: "https://abhinavsingh-steel.vercel.app/projects-achievements",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "daily",
       priority: 0.5,
     },
   ];
