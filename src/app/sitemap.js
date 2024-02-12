a9d7ece07665d6eee4c74944e8c82ece11a8be6d
Abhinav Singh <abhinavas430@gmail.com>
2024-02-12 15:21:33 +0530

    Update URLs in sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,25 +1,25 @@
 export default function sitemap() {
   return [
     {
-      url: "https://abhinavsingh-steel.vercel.app/",
+      url: "https://imabhinavsingh.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: "https://abhinavsingh-steel.vercel.app/about",
+      url: "https://imabhinavsingh.vercel.app/about",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
     },
     {
-      url: "https://abhinavsingh-steel.vercel.app/blog",
+      url: "https://imabhinavsingh.vercel.app/blog",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.5,
     },
     {
-      url: "https://abhinavsingh-steel.vercel.app/projects-achievements",
+      url: "https://imabhinavsingh.vercel.app/projects-achievements",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.5,
